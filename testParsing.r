c78f0616681d4f67f391a6d52c19199bb051e247
rflight79 <r.flight79@gmail.com>
2012-09-29 14:57:48 -0400

    put the figure replacement (for Bioinformatics anyway) into a function that results in a new html file with the figures actually included directly in the document

--- testParsing.r
+++ testParsing.r
@@ -3,33 +3,34 @@
 
 require(XML)
 
+# this function replaces the figures for bioinformatics journal
+replaceFigure <- function(inDiv){
+  tmpRef <- as.character(xpathApply(inDiv, path='a/@href')[[1]])
+  figDoc <- htmlParse(file=tmpRef)
+  figfigDivs <- getNodeSet(figDoc, path='//*[@class="fig-expansion"]')
+  tmp2 <- figfigDivs[[1]]
+  fullFig <- as.character(xpathApply(tmp2, path='a/@href')[[1]])
+  
+  downLoadLoc <- dirname(tmpRef)
+  figDownLoad <- file.path(downLoadLoc, fullFig)
+  
+  
+  dir.create(writeDir)
+  fullFile <- file.path(writeDir, fullFig)
+  download.file(url=figDownLoad, destfile=fullFile, mode="wb")
+  
+  chNodes <- xpathApply(inDiv, "*")
+  invisible(removeChildren(inDiv, kids=chNodes))
+  fig <- newXMLNode("img", attrs=c("src"=fullFile, "width"=800))
+  invisible(addChildren(inDiv, kids=list(fig)))
+}
+
+
 # work with the bioinformatics example
 hDoc <- htmlParse(file="testHTML/bioinformaticsTest.htm")
-
-figDivs <- getNodeSet(hDoc, path='//*[@class="fig-inline"]')
-
-# now for each figure Div, parse the linked page
-
-tmpDat <- figDivs[[1]]
-
-tmpRef <- as.character(xpathApply(tmpDat, path='a/@href')[[1]])
-
-figDoc <- htmlParse(file=tmpRef)
-figfigDivs <- getNodeSet(figDoc, path='//*[@class="fig-expansion"]')
-tmp2 <- figfigDivs[[1]]
-fullFig <- as.character(xpathApply(tmp2, path='a/@href')[[1]])
-
-downLoadLoc <- dirname(tmpRef)
-figDownLoad <- file.path(downLoadLoc, fullFig)
-
 writeDir <- "extraFiles"
-dir.create(writeDir)
-fullFile <- file.path(writeDir, fullFig)
-download.file(url=figDownLoad, destfile=fullFile, mode="wb")
 
-chNodes <- xpathApply(tmpDat, "*")
-removeChildren(tmpDat, kids=chNodes)
-fig <- newXMLNode("img", attrs=c("src"=fullFile, "width"=800))
-addChildren(tmpDat, kids=list(fig))
+figDivs <- xpathApply(hDoc, path='//*[@class="fig-inline"]', replaceFigure)
 
-saveXML(hDoc, file="testReplaceFig.html")
+saveXML(hDoc, file="test2.html")
+
